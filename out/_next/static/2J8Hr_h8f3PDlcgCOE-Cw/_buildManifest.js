e99980c44e03d58385ee892315731d544853a77c
Krunal "kAOS" Shinde <kaostics12321@gmail.com>
2023-10-27 18:57:54 +0530

    out folder commit

---
+++ out/_next/static/2J8Hr_h8f3PDlcgCOE-Cw/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/chunks/845-bef602fd86578c91.js","static/chunks/pages/index-f08eda3d3df42291.js"],"/_error":["static/chunks/pages/_error-8e848193de3c139c.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
