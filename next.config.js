2aa51b2e02b5090dfffa0075e04ac8b3fa799a53
Krunal "kAOS" Shinde <kaostics12321@gmail.com>
2023-10-27 19:18:18 +0530

    config commit

--- next.config.js
+++ next.config.js
@@ -13,7 +13,7 @@ const nextConfig = {
         unoptimized: true,
       },
     reactStrictMode: true,
-    basePath: process.env.NODE_ENV === "production" ? "kaos6-9.github.io/gamerverse": undefined,
+    basePath: process.env.NODE_ENV === "production" ? "/gamerverse": undefined,
 };
 
 module.exports = nextConfig
