2bbe6b8a94f987ff640abfc07e63884a38ef6ec0
Krunal "kAOS" Shinde <kaostics12321@gmail.com>
2023-10-27 18:43:01 +0530

    first commit

--- next.config.js
+++ next.config.js
@@ -1,15 +1,16 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    experimental: {
+    experimental:{
         appDir: false,
-    },
+    }
 };
 
-module.exports = {
-    nextConfig,
+module.exports = 
+{
+    nextConfig: nextConfig,
     eslint: {
         // Warning: This allows production builds to successfully complete even if
         // your project has ESLint errors.
         ignoreDuringBuilds: true,
-      },
+      }
 }
